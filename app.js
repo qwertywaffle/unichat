9f35017e8d604cd41c265ba290309f01b2fcf75d
qwerty <6lettersonurkeyboard@gmail.com>
2026-04-20 01:54:03 -0400

    dear copilot please stop typing bs in my commit

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ window.addEventListener('load', ()=>{
 });
 
 let ws;
-const SERVER_WS_URL = "ws://ullbn-2607-fb92-2a86-99-e82e-5ef0-6f0b-7d7a.run.pinggy-free.link:35273";
+const SERVER_WS_URL = "ws://cyezf-2607-fb92-2a86-99-e82e-5ef0-6f0b-7d7a.run.pinggy-free.link";
 // currently-editing message id
 let editingId = null;
 
